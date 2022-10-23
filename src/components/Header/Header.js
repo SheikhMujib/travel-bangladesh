8b30009423dc31923ee03f3a3f5fc39c76ade6b7
SheikhMujib <mbasheikhmujib@gmail.com>
2022-10-23 16:20:44 +0600

    Basic navbar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div>
-            Header
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Travel Bangladesh
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="anavbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/home" aria-current="page">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="a">
+                Link
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success me-2" type="submit">
+              Search
+            </button>
+            <button className="btn btn-outline-danger me-2" type="submit">
+              Login
+            </button>
+            <button className="btn btn-outline-success me-2" type="submit">
+              Signup
+            </button>
+          </form>
         </div>
-    );
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
