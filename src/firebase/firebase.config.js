71978e5640169b1259f0d813bf778904de2b3ff7
SheikhMujib <mbasheikhmujib@gmail.com>
2022-10-23 15:32:31 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCCDuc0vyoLwy9spUcB1PzT7oQBtRiw4k",
+  authDomain: "travel-bangladesh-55915.firebaseapp.com",
+  projectId: "travel-bangladesh-55915",
+  storageBucket: "travel-bangladesh-55915.appspot.com",
+  messagingSenderId: "392113214825",
+  appId: "1:392113214825:web:5530251c7272ae6cd959f8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
